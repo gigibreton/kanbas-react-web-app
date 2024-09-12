83b98f7979e57f61ae96131f0b7a9762e0e853b7
gigibreton <breton.b@northeastern.edu>
2024-09-12 12:10:05 -0400

    a1 lab finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Labs from './Labs';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <HashRouter>
     <div>
-      <h1>Welcome to Web Dev</h1>
-      <Labs />
+      <Routes>
+        <Route path="/" element={<Navigate to="Labs" />} />
+        <Route path="/Labs/*" element={<Labs />} />
+      </Routes>
     </div>
+    </HashRouter>
   );
 }
 
