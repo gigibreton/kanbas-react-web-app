6e398c5e1e52179ac9a44eff0d811435bfb65c50
gigibreton <breton.b@northeastern.edu>
2024-10-12 18:51:45 -0400

    2.4.3 completed

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,38 @@
+import AddingRemovingDataToFromArrays from "./AddingRemovingDataToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import ForLoops from "./ForLoops";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import LegacyFunctions from "./LegacyFunctions";
+import SimpleArrays from "./SimpleArrays";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
-    return(
-        <div>
-            <h2>Lab 3</h2>
+    return (
+        <div id="wd-lab3">
+            <h3>Lab 3</h3>
+            <VariablesAndConstants />
+            <VariableTypes />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputInline />
+            <LegacyFunctions />
+            <ArrowFunctions />
+            <ImpliedReturn />
+            <TemplateLiterals />
+            <SimpleArrays />
+            <ArrayIndexAndLength />
+            <AddingRemovingDataToFromArrays />
+            <ForLoops />
         </div>
     );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,7 +5,7 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 
 export default function Labs() {
-    return(
+    return (
         <div>
             <h4>Brigette Breton | CS5610, Section 01</h4>
             <h1>Labs</h1>
@@ -14,7 +14,7 @@ export default function Labs() {
                 <Route path="/" element={<Navigate to="Lab1" />} />
                 <Route path="Lab1" element={<Lab1 />} />
                 <Route path="Lab2" element={<Lab2 />} />
-                <Route path="Lab3" element={<Lab3 />} />
+                <Route path="Lab3/*" element={<Lab3 />} />
             </Routes>
         </div>
     );
