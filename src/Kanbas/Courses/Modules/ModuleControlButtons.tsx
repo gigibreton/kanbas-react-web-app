1338c70a827dda83099b80043ae44d02f2f7db09
gigibreton <breton.b@northeastern.edu>
2024-10-25 19:38:55 -0400

    3.3.4 complete

--- src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
+++ src/Kanbas/Courses/Modules/ModuleControlButtons.tsx
@@ -11,10 +11,4 @@ export default function LessonControlButtons(
     return (
         <div className="float-end">
             <FaPencil onClick={() => editModule(moduleId)} className="text-primary me-3" />
-            <FaTrash className="text-danger me-2 mb-1" onClick={() => deleteModule(moduleId)} />
-            <GreenCheckmark />
-            <BsPlus />
-            <IoEllipsisVertical className="fs-4" />
-        </div>
-    );
-}
+            <FaTrash
