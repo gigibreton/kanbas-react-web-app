26b30fdb5e14ae930d14d3bd6b931da519e4fb22
Gigi Breton <breton.b@northeastern.edu>
2024-11-13 16:42:39 -0500

    up to 4.4

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -2,16 +2,22 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
+import * as client from "./client";
 export default function Profile() {
     const [profile, setProfile] = useState<any>({});
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const updateProfile = async () => {
+        const updatedProfile = await client.updateUser(profile);
+        dispatch(setCurrentUser(updatedProfile));
+    };
     const fetchProfile = () => {
         if (!currentUser) return navigate("/Kanbas/Account/Signin");
         setProfile(currentUser);
     };
-    const signout = () => {
+    const signout = async () => {
+        await client.signout();
         dispatch(setCurrentUser(null));
         navigate("/Kanbas/Account/Signin");
     };
@@ -26,13 +32,13 @@ export default function Profile() {
                     <input defaultValue={profile.password} id="wd-password" className="form-control mb-2"
                         onChange={(e) => setProfile({ ...profile, password: e.target.value })} />
                     <input defaultValue={profile.firstName} id="wd-firstname" className="form-control mb-2"
-                        onChange={(e) => setProfile({ ...profile, firstName: e.target.value })} />
+                        placeholder="First Name" onChange={(e) => setProfile({ ...profile, firstName: e.target.value })} />
                     <input defaultValue={profile.lastName} id="wd-lastname" className="form-control mb-2"
-                        onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
+                        placeholder="Last Name" onChange={(e) => setProfile({ ...profile, lastName: e.target.value })} />
                     <input defaultValue={profile.dob} id="wd-dob" className="form-control mb-2"
                         onChange={(e) => setProfile({ ...profile, dob: e.target.value })} type="date" />
                     <input defaultValue={profile.email} id="wd-email" className="form-control mb-2"
-                        onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
+                        placeholder="Email" onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
                     {profile.role === 'FACULTY' && (
                         <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
                             defaultValue={profile.role}
@@ -43,6 +49,7 @@ export default function Profile() {
                             <option value="STUDENT">Student</option>
                         </select>
                     )}
+                    <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
                     <button onClick={signout} className="btn btn-danger w-100 mb-2" id="wd-signout-btn">
                         Sign out
                     </button>
