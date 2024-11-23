1e6a9d457eedca5c4c3f017e9d6367b37d5c348a
Gigi Breton <breton.b@northeastern.edu>
2024-11-23 01:06:17 -0500

    up to 4.0, not all working

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -19,6 +19,14 @@ export const findUserById = async (id: string) => {
     const response = await axios.get(`${USERS_API}/${id}`);
     return response.data;
 };
+export const createUser = async (user: any) => {
+    const response = await axios.post(`${USERS_API}`, user);
+    return response.data;
+};
+export const deleteUser = async (userId: string) => {
+    const response = await axios.delete(`${USERS_API}/${userId}`);
+    return response.data;
+};
 export const findMyCourses = async () => {
     const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
     return data;
